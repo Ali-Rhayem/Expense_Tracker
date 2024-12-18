8ae6438f2b29ef9ba6497db3af69b1cf71bb6b7d
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-18 21:23:32 +0200

    Refactor title input handling in NewExpense widget to use TextEditingController

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,11 +9,13 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = "";
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
-  }
+  final _titleController = TextEditingController();
 
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,7 +23,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text("Title"),
@@ -31,7 +33,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                   onPressed: () {
-                    print(_enteredTitle);
+                    print(_titleController.text);
                   },
                   child: const Text("save expense")),
             ],
