371ee18558dbed971646473a4ec627f2c80f0e7b
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-18 20:51:07 +0200

    Add Material3 theme and AppBar to Expenses widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,9 +29,14 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expense Tracker"),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.add) )
+        ],
+      ),
       body: Column(
         children: [
-          //
           const Text('the chart'),
           Expanded(
             child: ExpensesList(expenses: _registeredExpenses),
