82938d75cc27e299fbd58e3a186d4ca406047a59
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-23 22:56:07 +0200

    Add ExpenseBucket model and fix margin access in ExpensesList widget

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -33,3 +33,17 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (var expense in expenses){
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
